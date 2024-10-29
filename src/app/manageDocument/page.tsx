c41e353229e1c8f0bcb2d967d963cc2509473beb
nayeong lee <lny021102@gmail.com>
2024-10-29 16:43:06 +0900

    chore: 모든 작업 폴더 생성

---
+++ src/app/manageDocument/page.tsx
@@ -0,0 +1,10 @@
+// 서류관리 화면
+// 담당자(담당 브랜치): yerim
+
+export default function Home() {
+	return (
+		<div>
+			서류관리 화면
+		</div>
+	);
+}
---
+++ src/app/mozipFormCreate/page.tsx
@@ -0,0 +1,10 @@
+// 모집 폼 생성 화면
+// 담당자(담당 브랜치): nayeong
+
+export default function Home() {
+	return (
+		<div>
+			모집 폼 생성 화면
+		</div>
+	);
+}
