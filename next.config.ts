1df894cb06c9962dbedf5087a802e22f3b88588d
nayeong lee <lny021102@gmail.com>
2024-10-29 19:38:11 +0900

    chore: styled component 사용을 위한 설정 추가

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
+import { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  compiler: {
+    // styled-components 지원 활성화
+    styledComponents: true,
+  },
 };
 
 export default nextConfig;
