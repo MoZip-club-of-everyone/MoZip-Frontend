f7416c4007e1b4dcdd75f553d86776cdf3bf4fbc
wiseah <hyunah2765@gmail.com>
2024-11-05 22:19:55 +0900

    Merge: 현아 + 나영

--- src/app/homepage/page.tsx
+++ src/app/homepage/page.tsx
@@ -1,10 +1,18 @@
+"use client";
+
 // 로딩 시 첫화면 (메인화면)
 // 담당자(담당 브랜치): hyuna
+import HomeButton from "./components/HomeButton";
 
 export default function Home() {
+	const handleLoginClick = () => {
+		console.log('Login/Logout clicked');
+	  };
+
 	return (
 		<div>
 			여기는 홈페이지 입니다.
+			<HomeButton isLoggedIn={false} onClick={handleLoginClick} />
 		</div>
 	);
 }
