a64cd5d915d550f4fc10ae6e34fb29b9706bf114
nayeong lee <lny021102@gmail.com>
2024-11-07 19:46:21 +0900

    fix(MozipTabs): 모든 탭을 버튼화

--- src/app/mozipFormCreate/MozipTabs/MozipTabs.tsx
+++ src/app/mozipFormCreate/MozipTabs/MozipTabs.tsx
@@ -4,6 +4,7 @@ import styled from "styled-components";
 import CustomFont from "@/components/CustomFont";
 import CustomRow from "@/components/CustomRow";
 import StyledImg from "@/components/StyledImg";
+import CustomButton from "@/components/CustomButton";
 import { IoIosArrowForward } from "react-icons/io";
 
 // 담당자: 나영
@@ -20,22 +21,28 @@ export default function MozipTabs() {
 
 		<CustomTabs $width="100%" $alignitems="center" $justifycontent="flex-start" $gap="1rem">
 
-			<CustomRow $width="auto" $alignitems="center" $justifycontent="center" $gap="0.5rem">
-				<CustomFont $color='black' $font="1.2rem" $fontweight="bold">설명 작성</CustomFont>
-				<StyledImg src={'/icon_TabStar.png'} $width='1rem' $height="1rem" />
-			</CustomRow>
+			<CustomButton $width='auto' $backgroundColor="transparent" $padding='0'>
+				<CustomRow $width="auto" $alignitems="center" $justifycontent="center" $gap="0.5rem">
+					<CustomFont $color='black' $font="1.2rem" $fontweight="bold">설명 작성</CustomFont>
+					<StyledImg src={'/icon_TabStar.png'} $width='1rem' $height="1rem" />
+				</CustomRow>
+			</CustomButton>
 
 			<IoIosArrowForward />
 
-			<CustomRow $width="auto" $alignitems="center" $justifycontent="center" $gap="0.5rem">
-				<CustomFont $color='black' $font="1.2rem" $fontweight="bold">질문 작성</CustomFont>
-			</CustomRow>
+			<CustomButton $width='auto' $backgroundColor="transparent" $padding='0'>
+				<CustomRow $width="auto" $alignitems="center" $justifycontent="center" $gap="0.5rem">
+					<CustomFont $color='black' $font="1.2rem" $fontweight="bold">질문 작성</CustomFont>
+				</CustomRow>
+			</CustomButton>
 
 			<IoIosArrowForward />
 
-			<CustomRow $width="auto" $alignitems="center" $justifycontent="center" $gap="0.5rem">
-				<CustomFont $color='black' $font="1.2rem" $fontweight="bold">설정</CustomFont>
-			</CustomRow>
+			<CustomButton $width='auto' $backgroundColor="transparent" $padding='0'>
+				<CustomRow $width="auto" $alignitems="center" $justifycontent="center" $gap="0.5rem">
+					<CustomFont $color='black' $font="1.2rem" $fontweight="bold">설정</CustomFont>
+				</CustomRow>
+			</CustomButton>
 
 		</CustomTabs>
 	);
