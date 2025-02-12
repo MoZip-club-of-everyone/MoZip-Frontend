d57680a824de333b55d7fb4867c8b8df829053a9
nayeong lee <lny021102@gmail.com>
2025-02-13 06:25:59 +0900

    fix: mozipFormCreate 파일 레이아웃 수정

--- src/app/mozipFormCreate/MozipLayout/header.tsx
+++ src/app/mozipFormCreate/MozipLayout/header.tsx
@@ -6,6 +6,7 @@ import CustomDivider from "@/components/CustomDivider";
 import CustomRow from "@/components/CustomRow";
 import CustomColumn from "@/components/CustomColumn";
 import CustomButton from "@/components/CustomButton";
+import { useRouter } from "next/navigation";
 
 import Image from 'next/image';
 import headerLogo from '@/assets/logo/headerLogo.svg';
@@ -19,17 +20,18 @@ const CustomHeader = styled(CustomColumn)`
 `;
 
 export default function Header() {
+	const router = useRouter();
+
+	const GoMain = () => {
+		router.push("/homepage");
+	}
 
 	return (
 		<CustomHeader $width="100%" $gap="0" $alignitems="flex-end">
-			<CustomRow $width="90%" $padding="0.5rem" $justifycontent="space-between" $alignitems="center">
-				<CustomButton $width='auto' $backgroundColor="transparent" $padding="1rem">
+			<CustomRow $width="90%" $padding="0.5rem" $justifycontent="flex-start" $alignitems="center">
+				<CustomButton $width='auto' $backgroundColor="transparent" $padding="1rem" onClick={GoMain}>
 					{/* <StyledImg src={headerLogo} width='4rem' /> */}
 					<Image src={headerLogo} alt='logo' />
-				</CustomButton>
-
-				<CustomButton $width='auto' $backgroundColor="transparent">
-					<CustomFont $color="black" $font="0.8rem">로그인</CustomFont>
 				</CustomButton>
 			</CustomRow>
 			<CustomDivider $width="100%" $height="1px" $backgroundcolor="#D8D8D8" />
